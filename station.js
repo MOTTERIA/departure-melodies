29edc174640458916a6995226afda77d91fb15c5
MOTTERIA <tjiawutian@gmail.com>
2026-04-03 00:30:00 +0900

    Update station.js

--- station.js
+++ station.js
@@ -3,14 +3,119 @@ const stations = [
     name: "会津大塩",
     kana: "あいづおおしお",
     roman: "Aizu-Ōshio",
-    lines: ["左沢線"],
+    lines: ["只見線"],
     file: "stations/aizuoshio.html"
   },
   {
     name: "会津蒲生",
     kana: "あいづがもう",
     roman: "Aizu-Gamō",
-    lines: ["左沢線"],
+    lines: ["只見線"],
     file: "stations/aizugamo.html"
-  }
+  },
+   {
+    name: "会津川口",
+    kana: "あいづかわぐち",
+    roman: "Aizu-Kawaguchi",
+    lines: ["只見線"],
+    file: "stations/aizukawaguchi.html"
+  },
+   {
+    name: "会津越川",
+    kana: "あいづこすがわ",
+    roman: "Aizu-Kosugawa",
+    lines: ["只見線"],
+    file: "stations/aizukosugawa.html"
+  },
+   {
+    name: "会津坂本",
+    kana: "あいづさかもと",
+    roman: "Aizu-Sakamoto",
+    lines: ["只見線"],
+    file: "stations/aizusakamoto.html"
+  },
+   {
+    name: "会津塩沢",
+    kana: "あいづしおざわ",
+    roman: "Aizu-Shiozawa",
+    lines: ["只見線"],
+    file: "stations/aizushiozawa.html"
+  },
+   {
+    name: "会津高田",
+    kana: "あいづたかだ",
+    roman: "Aizu-Takada",
+    lines: ["只見線"],
+    file: "stations/aizutakada.html"
+  },
+   {
+    name: "会津豊川",
+    kana: "あいづとよかわ",
+    roman: "Aizu-Toyokawa",
+    lines: ["只見線"],
+    file: "stations/aizutoyokawa.html"
+  },
+   {
+    name: "会津中川",
+    kana: "あいづなかがわ",
+    roman: "Aizu-Nakagawa",
+    lines: ["只見線"],
+    file: "stations/aizunakagawa.html"
+  },
+  {
+    name: "会津西方",
+    kana: "あいづにしかた",
+    roman: "Aizu-Nishikata",
+    lines: ["只見線"],
+    file: "stations/aizuNishikata.html"
+  },
+  {
+    name: "会津坂下",
+    kana: "あいづばんげ",
+    roman: "Aizu-Bange",
+    lines: ["只見線"],
+    file: "stations/aizubange.html"
+  },
+  {
+    name: "会津桧原",
+    kana: "あいづひのはら",
+    roman: "Aizu-Hinohara",
+    lines: ["只見線"],
+    file: "stations/aizuhinohara.html"
+  },
+  {
+    name: "会津本郷",
+    kana: "あいづほんごう",
+    roman: "Aizu-Hongō",
+    lines: ["只見線"],
+    file: "stations/aizuhongo.html"
+  },
+  {
+    name: "会津水沼",
+    kana: "あいづみずぬま",
+    roman: "Aizu-Mizunuma",
+    lines: ["只見線"],
+    file: "stations/aizumizunuma.html"
+  },
+  {
+    name: "会津柳津",
+    kana: "あいづやないづ",
+    roman: "Aizu-Yanaizu",
+    lines: ["只見線"],
+    file: "stations/aizuyanaizu.html"
+  },
+  {
+    name: "会津横田",
+    kana: "あいづよこた",
+    roman: "Aizu-Yokota",
+    lines: ["只見線"],
+    file: "stations/aizuyokota.html"
+  },
+  {
+    name: "会津若松",
+    kana: "あいづわかまつ",
+    roman: "Aizu-Wakamatsu",
+    lines: ["只見線","磐越西線"],
+    file: "stations/aizuwakamatsu.html"
+  },
 ];
