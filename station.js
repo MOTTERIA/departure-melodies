531394d65890f40a18de12b0bb3575f715bc9513
MOTTERIA <tjiawutian@gmail.com>
2026-04-02 20:26:49 +0900

    Update station.js

---
+++ station.js
@@ -0,0 +1,14 @@
+const stations = [
+  {
+    name: "会津大塩",
+    kana: "あいづおおしお",
+    lines: ["左沢線"],
+    file: "stations/aizuoshio.html"
+  },
+  {
+    name: "会津蒲生",
+    kana: "あいづがもう",
+    lines: ["左沢線"],
+    file: "stations/aizugamo.html"
+  }
+];
