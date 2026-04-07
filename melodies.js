23ef6e4c3700c4965f76582751b5b7f39346c479
MOTTERIA <tjiawutian@gmail.com>
2026-04-08 01:04:57 +0900

    Create melodies.js

---
+++ melodies.js
@@ -0,0 +1,9 @@
+const melodies = [
+  {
+    name: "JRE-IKST-001",
+    kana: "じぇいあーるいー あいけーえすてぃー ぜろぜろいち",
+    roman: "JRE-IKST-001",
+    file: "JRE-IKST-001.html",
+    category: "発車メロディ",
+  ]
+];
